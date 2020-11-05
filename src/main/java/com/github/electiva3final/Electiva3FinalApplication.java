b141806899bb47c5cf1819df35d88f7a630f8da8
Javier Heisecke <javierheiseckee@gmail.com>
2020-11-05 20:12:05 -0300

    Inicializa proyecto

---
+++ src/main/java/com/github/electiva3final/Electiva3FinalApplication.java
@@ -0,0 +1,13 @@
+package com.github.electiva3final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Electiva3FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Electiva3FinalApplication.class, args);
+	}
+
+}
